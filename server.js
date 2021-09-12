7b943400cf535a6bce8ecbdea98c2db85dbfa4c6
thephotogenicbug <navinkumarachari@gmail.com>
2021-09-12 17:25:48 +0530

    MERN : Add user register and login functionality and hash the password using bcrypt and

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 dotenv.config();
 app.use(cors());
-
-// Connect to DB
 connectDB();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Api is Running...");
@@ -20,13 +21,17 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-// get notes by id
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-  res.send(note);
-  //   console.log(req.params);
-});
+// // get notes by id
+// app.get("/api/notes/:id", (req, res) => {
+//   const note = notes.find((n) => n._id === req.params.id);
+//   res.send(note);
+//   //   console.log(req.params);
+// });
 
+app.use("/api/users", userRoutes);
+
+app.use(notFound)
+app.use(errorHandler)
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
