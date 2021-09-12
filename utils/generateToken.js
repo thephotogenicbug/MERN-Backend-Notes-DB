42bcc836ab9939514135a2788cd98282266ba9b2
thephotogenicbug <navinkumarachari@gmail.com>
2021-09-12 17:33:45 +0530

    Add Jwt Token for client

---
+++ utils/generateToken.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken')
+
+const generateToken = (id) =>{
+
+    return jwt.sign({id}, process.env.JWT_SECERET, {
+        expiresIn: "30d"
+    })
+}
+
+module.exports = generateToken
