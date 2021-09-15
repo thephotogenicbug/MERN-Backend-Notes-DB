a49796fe06bea391a3d32fb990bf88a3609f0d26
thephotogenicbug <navinkumarachari@gmail.com>
2021-09-15 18:00:27 +0530

    MERN : backend notes api implement notes post delete put functions

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
@@ -16,22 +16,11 @@ app.get("/", (req, res) => {
   res.send("Api is Running...");
 });
 
-// get all notes
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
-
-// // get notes by id
-// app.get("/api/notes/:id", (req, res) => {
-//   const note = notes.find((n) => n._id === req.params.id);
-//   res.send(note);
-//   //   console.log(req.params);
-// });
-
 app.use("/api/users", userRoutes);
+app.use("/api/notesapi", noteRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
